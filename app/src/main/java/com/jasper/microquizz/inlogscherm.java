a7509d233592f4ded1647c6380abfeba47c632aa
JasperJJ <36187266+JasperJJ@users.noreply.github.com>
2019-11-22 14:42:59 +0100

    Changed Inloggen colors

--- app/src/main/java/com/jasper/microquizz/inlogscherm.java
+++ app/src/main/java/com/jasper/microquizz/inlogscherm.java
@@ -1,14 +1,41 @@
 package com.jasper.microquizz;
 
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class inlogscherm extends AppCompatActivity {
 
+    private Button btn_inloggen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inlogscherm);
+
+        findByID();
+        setBackGroundColors();
+
+        btn_inloggen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void findByID() {
+        btn_inloggen = findViewById(R.id.btn_inloggen);
+    }
+
+    public void setBackGroundColors() {
+        GradientDrawable btn_inloggen_bg = (GradientDrawable) btn_inloggen.getBackground();
+
+        btn_inloggen_bg.setColor(getResources().getColor(R.color.colorBlue));
     }
 }
