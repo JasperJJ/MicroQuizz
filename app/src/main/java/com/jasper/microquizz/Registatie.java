9616f5d7f278c18257f05e91e2998a6be0730152
JasperJJ <36187266+JasperJJ@users.noreply.github.com>
2019-11-28 16:08:26 +0100

    Fixed login and registration errors

--- app/src/main/java/com/jasper/microquizz/Registatie.java
+++ app/src/main/java/com/jasper/microquizz/Registatie.java
@@ -1,14 +1,30 @@
 package com.jasper.microquizz;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registatie extends AppCompatActivity {
 
+    private Button btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registatie);
+
+        findByID();
+        setBackGroundColors();
+    }
+    public void findByID() {
+        btn_register = findViewById(R.id.btn_register);
+    }
+
+    public void setBackGroundColors() {
+        GradientDrawable btn_register_bg = (GradientDrawable) btn_register.getBackground();
+
+        btn_register_bg.setColor(getResources().getColor(R.color.colorBlue));
     }
 }
