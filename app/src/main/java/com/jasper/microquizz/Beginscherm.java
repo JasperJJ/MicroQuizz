d83b2683aaa0ab46795b6772275a14750ccd7ea3
DaniquevW <Danwinden@gmail.com>
2019-11-29 10:14:22 +0100

    beginscherm

---
+++ app/src/main/java/com/jasper/microquizz/Beginscherm.java
@@ -0,0 +1,14 @@
+package com.jasper.microquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Beginscherm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginscherm);
+    }
+}
