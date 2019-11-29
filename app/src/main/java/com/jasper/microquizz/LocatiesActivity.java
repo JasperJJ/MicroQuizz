850c8101be94be00fa8d933da4b4b04b1f76ca16
JasperJJ <36187266+JasperJJ@users.noreply.github.com>
2019-11-29 13:25:29 +0100

    Placeholder Text

--- app/src/main/java/com/jasper/microquizz/LocatiesActivity.java
+++ app/src/main/java/com/jasper/microquizz/LocatiesActivity.java
@@ -1,17 +1,28 @@
 package com.jasper.microquizz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 import com.jasper.microquizz.adapters.locatieAdapter;
 
 import java.util.ArrayList;
 
 public class LocatiesActivity extends AppCompatActivity implements locatieAdapter.ItemClickListener{
+
+    private DrawerLayout drawerLayout;
+
     private RecyclerView recyclerView;
     private locatieAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -22,6 +33,9 @@ public class LocatiesActivity extends AppCompatActivity implements locatieAdapte
         setContentView(R.layout.activity_locaties);
         recyclerView = findViewById(R.id.my_recycler_view);
 
+        configureNavigationDrawer();
+        configureToolbar();
+
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
@@ -31,15 +45,58 @@ public class LocatiesActivity extends AppCompatActivity implements locatieAdapte
         recyclerView.setLayoutManager(layoutManager);
 
         ArrayList<String> list = new ArrayList<>();
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
+        list.add("Een korte beschrijving van het schilderij of het beeld");
+        list.add("Een korte beschrijving van het schilderij of het beeld");
+        list.add("Een korte beschrijving van het schilderij of het beeld");
+        list.add("Een korte beschrijving van het schilderij of het beeld");
+        list.add("Een korte beschrijving van het schilderij of het beeld");
 
         // specify an adapter (see also next example)
         mAdapter = new locatieAdapter(this, list);
         mAdapter.setClickListener(this);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_hamburger_menu);
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void configureNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navView = findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.action_home) {
+                    Intent intent = new Intent(LocatiesActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.action_musea) {
+                    Intent intent = new Intent(LocatiesActivity.this, LocatiesActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return true;
     }
 
     @Override
