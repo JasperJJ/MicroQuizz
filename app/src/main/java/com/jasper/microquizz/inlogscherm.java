f46aff5c775397fe6d5912cd60e6b1cbcd83535b
Timdt <timdetombe@gmail.com>
2019-12-16 16:42:55 +0100

    Login functie toegevoegd

--- app/src/main/java/com/jasper/microquizz/inlogscherm.java
+++ app/src/main/java/com/jasper/microquizz/inlogscherm.java
@@ -5,6 +5,8 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +14,29 @@ public class inlogscherm extends AppCompatActivity {
 
     private Button btn_inloggen;
 
+    private EditText naam;
+    private EditText wachtwoord;
+    private Button login;
+    private TextView inlogpoging;
+    //teller voor inlogpogingen
+    private int loginTeller = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inlogscherm);
 
-        findByID();
-        setBackGroundColors();
+
+        //definieer de buttons in inlogscherm xml bij id.
+        naam = (EditText)findViewById(R.id.et_naam);
+        wachtwoord = (EditText)findViewById(R.id.et_wachtwoord);
+        login = (Button)findViewById(R.id.btn_inloggen);
+        inlogpoging = (TextView)findViewById(R.id.tv_inlogpoging);
+
+        inlogpoging.setText("Aantal pogingen over: 5");
+
+        //findByID();
+        //setBackGroundColors();
 
         btn_inloggen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +51,34 @@ public class inlogscherm extends AppCompatActivity {
         btn_inloggen = findViewById(R.id.btn_inloggen);
     }
 
-    public void setBackGroundColors() {
-        GradientDrawable btn_inloggen_bg = (GradientDrawable) btn_inloggen.getBackground();
+   // public void setBackGroundColors() {
+    //    GradientDrawable btn_inloggen_bg = (GradientDrawable) btn_inloggen.getBackground();
 
-        btn_inloggen_bg.setColor(getResources().getColor(R.color.colorBlue));
+     //   btn_inloggen_bg.setColor(getResources().getColor(R.color.colorBlue));
     }
+
+    //functie voor het inloggen te verifieren
+
+    private void inlogBevestig(String naam, String wachtwoord) {
+        //check of gebruikersnaam en wachtwoord kloppen
+        if ((naam.equals("Admin")) && (wachtwoord.equals("1234"))) {
+        //ga naar home scherm
+        Intent intent = new Intent(inlogscherm.this, HomeActivity.class);
+        startActivity(intent);
+        }
+        // als het wachtwoord niet klopt doe dan dit ( else )
+        else {
+            // hier tellen we de inlogpogingen
+            loginTeller--;
+            inlogpoging.setText("Aantal pogingen over: " + String.valueOf(loginTeller));
+            if (loginTeller == 0) {
+                login.setEnabled(false);
+
+            }
+
+        }
+
+    }
+
+
 }
