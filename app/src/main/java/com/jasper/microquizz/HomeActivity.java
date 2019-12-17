5fe05c28fe5edbe7531b41db0358309f34f5ef89
Timdt <timdetombe@gmail.com>
2019-12-17 21:35:16 +0100

    App draait niet, de switch is niet goed, iets in het menu is niet goed.

--- app/src/main/java/com/jasper/microquizz/HomeActivity.java
+++ app/src/main/java/com/jasper/microquizz/HomeActivity.java
@@ -3,6 +3,7 @@ package com.jasper.microquizz;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,13 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
 
     private DrawerLayout drawerLayout;
 
@@ -31,11 +37,48 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        logout = (Button)findViewById(R.id.btn_highscore);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logout();
+            }
+        });
+
+
+
         initControl();
         setBackGroundColors();
         configureNavigationDrawer();
         configureToolbar();
     }
+
+    private void Logout(){
+
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(HomeActivity.this, inlogscherm.class));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //@Override
+    //public  boolean onOptionsItemSelected(MenuItem item) {
+        //switch(item.getItemId())
+        //case (R.id.btn_highscore) {
+          //  Logout();
+        //}
+      //  return super.onOptionsItemSelected(item);
+    //}
+
 
     private void configureToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
