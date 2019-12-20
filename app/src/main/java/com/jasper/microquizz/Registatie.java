e3a7fe06591b1c1ccd9a14d2f23cf2a8928096f2
Timdt <timdetombe@gmail.com>
2019-12-20 11:22:17 +0100

    Registratie aangemaakt, werkt nog niet -->

--- app/src/main/java/com/jasper/microquizz/Registatie.java
+++ app/src/main/java/com/jasper/microquizz/Registatie.java
@@ -5,17 +5,50 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class Registatie extends AppCompatActivity {
 
     private Button btn_register;
 
+    private EditText userName, userPassword, userEmail;
+    private Button regButton;
+    private TextView userLogin;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registatie);
+        setupUIViews();
+
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    //upload data naar de database
+                }
+            }
+        });
+
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registatie.this,inlogscherm.class ));
+            }
+        });
+
 
         findByID();
         setBackGroundColors();
@@ -27,9 +60,43 @@ public class Registatie extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+        private void setupUIViews() {
+            userName = (EditText)findViewById(R.id.etUserName);
+            userPassword = (EditText)findViewById(R.id.et_wachtwoord);
+            userEmail = (EditText)findViewById(R.id.etUserEmail);
+            regButton = (Button) findViewById(R.id.btnRegister);
+            userLogin = (TextView)findViewById(R.id.tvUserLogin);
+
+        }
+
+
+
+        private Boolean validate() {
+
+                Boolean result = false;
+                String name = userName.getText().toString();
+                String password = userPassword.getText().toString();
+                String email = userEmail.getText().toString();
+
+                if (name.isEmpty() || password.isEmpty() || email.isEmpty()) {
+
+                    Toast.makeText(Registatie.this, "Voer alstublieft alles in",Toast.LENGTH_SHORT).show();
+
+                } else {
+                        result = true;
+
+                }
+
+                return result;
+        }
+
+
+
     public void findByID() {
-        btn_register = findViewById(R.id.btn_register);
+        btn_register = findViewById(R.id.btnRegister);
     }
 
     public void setBackGroundColors() {
